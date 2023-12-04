dce744f02cccdc89bce59360d5a361e52ce7ed72
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2023-12-04 11:03:36 -0400

    Configuracion inicial del proyecto

---
+++ naranja-frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-orange-500'>
+        Naranja Frontend
+      </h1>
+      <p className="read-the-docs">
+        Primera pagina del proyecto.
+      </p>
+    </>
+  )
+}
+
+export default App
