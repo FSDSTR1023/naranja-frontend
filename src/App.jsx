af506e95fee8acc91dde89a890da7bac867c7c07
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2023-12-04 11:29:33 -0400

    reinitialized repository

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <h1 className='text-orange-500'>
-        Naranja Frontend Group
+        Naranja Frontend
       </h1>
       <p className="read-the-docs">
         Primera pagina del proyecto.
