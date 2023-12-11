c8505b1d4bed124c0d4e8ed8d10f3a1b2d750017
22guerrabruno <22guerrabruno@gmail.com>
2023-12-11 18:22:58 +0100

    page 404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,23 @@
+import { useNavigate } from 'react-router-dom';
 const NavBar = () => {
-    return (
-       <div>404</div>
-    );
-  };
-  
-  export default NavBar;
-  
+  const navigate = useNavigate();
+  return (
+    <>
+      <div className='flex flex-col justify-center items-center w-full h-full gap-2'>
+        <h1 className='text-orange-400 text-9xl'>404 </h1>
+        <h2 className='text-4xl w-60 justify-end text-end'>Not Found</h2>
+        <p className='text-2xl mb-10'>
+          Looks like you are lost. The page you are looking for is no longer
+          here.
+        </p>
+        <button
+          onClick={() => navigate('/')}
+          className='bg-orange-400 text-black cursor-pointer'>
+          Home Page
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default NavBar;
