36f0c84e408cba0925dcac0da286cafc0d4dc8b9
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-01-16 14:15:39 +0100

    mas estilos del groupCard

--- src/components/GroupCard.jsx
+++ src/components/GroupCard.jsx
@@ -3,18 +3,20 @@
 const GroupCard = ({ group }) => {
   return (
     <div
-      className='flex flex-col border-2 border-gray-400 rounded-md cursor-pointer'
+      className='flex flex-col border-2 border-gray-400 rounded-md cursor-pointer w-full p-2 mx-2'
       onClick={() => {
         console.log('click en el grupo');
         alert(group._id);
       }}>
       <p className='font-bold text-sm'>{group?.name}</p>
-      <p className='font-bold text-sm'>{group?.description}</p>
-      <p className='font-bold text-sm'>Fecha último mensaje </p>
+      <p className='font-bold text-xs text-gray-700'>
+        Descripcion:{group?.description}
+      </p>
+
       {group.members.map((member) => (
         <span
           key={member?._id}
-          className='border-2 border-gray-300 p-1 text-[15px]'>
+          className='border-2 border-gray-300 p-1 mt-3 text-[15px] rounded-md'>
           {member?.name}
         </span>
       ))}
