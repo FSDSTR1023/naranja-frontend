c232f7bdde34c1875a4f86a72e8f2ecb1e31c1a3
galizaragoza <galizaragozam@gmail.com>
2024-01-22 15:12:41 +0100

    Cambios en VerifyEmailPager

--- src/pages/VerifyEmailPage.jsx
+++ src/pages/VerifyEmailPage.jsx
@@ -1,11 +1,10 @@
 import { Link } from 'react-router-dom';
+
 const VerifyEmailPage = () => {
   return (
-    <div>
-      Verifica tu correo electronico para activar tu cuenta{' '}
-      <Link to='/login-page'>
-        <span className='text-indigo-500'>Go to Login</span>
-      </Link>
+    <div className='flex flex-col justify-center max-w-[50%] mx-auto mt-40 '>
+      <h1 className='text-2xl font-bold mb-4'>Busca en tu bandeja de entrada el correo electrónico de verificación de TaskTalk y haz click en el enlace que contiene para verificar tu cuenta.</h1>
+      <Link to='/login-page' className='text-blue-700 font-bold underline'>Haz click aqui para ir al login</Link>
     </div>
   );
 };
