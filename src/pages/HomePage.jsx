c0354b4929f29f909f6b9f47b9aefa9ced2d84c8
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2024-01-22 18:10:05 -0400

    Modal
    created basic modal in order to use in all the app.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,20 @@
+import { useState } from 'react';
+import Modal from "../components/Modal";
+
 const HomePage = () => {
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const toggleModal = () => setIsModalVisible(!isModalVisible);
+
   return (
     <div className="my-10">
       <h1 className='text-orange-500'>Naranja Frontend</h1>
       <p className='read-the-docs'>Primera pagina del proyecto.</p>
+
+      <button onClick={toggleModal}>Abrir Modal</button>
+      <Modal isVisible={isModalVisible} onClose={toggleModal}>
+        <p>Prueba</p>
+      </Modal>
     </div>
   );
 };
