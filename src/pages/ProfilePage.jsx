b40d6204b6460a33cc292cf98209d9adacf88a21
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2024-02-04 21:59:03 -0400

    Update ProfilePage.jsx
    Creacion de un slider para agregar responsiveness en dispositivos pequeños.

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,12 +1,8 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import { useEffect } from 'react';
-
+import { useEffect, useState } from 'react';
 import { useUser } from '../context/UserContext';
-
 import { useNavigate } from 'react-router-dom';
-
 import { useGroups } from '../context/GroupContext';
-
 import ButtonDropDown from '../components/ButtonDropDown';
 import { TiMessages } from 'react-icons/ti';
 import PrivateChatCard from '../components/PrivateChatCard';
@@ -28,6 +24,11 @@ const ProfilePage = () => {
 
   const { getAllGroups, groups, privateGroups } = useGroups();
   const { room } = useMessage();
+  const [panelOpen, setPanelOpen] = useState(true);
+
+  const togglePanel = () => {
+    setPanelOpen(!panelOpen);
+  };
 
   const navigate = useNavigate();
 
@@ -36,20 +37,49 @@ const ProfilePage = () => {
       navigate('/');
       setUser(null);
       setIsAuthenticated(false);
-
       return;
     }
     getAllUsers();
-
     getAllGroups(user?._id);
   }, [user, usersChanges, room]);
 
   return (
     <div className='grid grid-cols-12 h-[100vh] bg-gray-900'>
+      {/* CHAT GRANDE */}
+      <div
+        className={`col-span-${panelOpen ? '9' : '12'} flex flex-col items-center justify-start mb-1 overflow-auto p-4 sm:p-0 sm:mb-0`}
+      >
+        <ChatPage />
+      </div>
+
+      {/* Button to open/close the panel */}
+      <button
+        onClick={togglePanel}
+        className={`bg-orange-500 absolute mt-[0px] right-0 px-3 py-4 z-10 text-white text-xs rounded-lg rounded-r-none ${
+          panelOpen ? 'hidden' : ''
+        }`}
+      >
+        Abrir
+      </button>
+      <button
+        onClick={togglePanel}
+        className={`absolute mt-[7px] right-4 z-10 text-white text-2xl ${
+          panelOpen ? '' : 'hidden'
+        }`}
+      >
+        x
+      </button>
+
       {/* Panel */}
-      <div className='col-span-3 overflow-auto p-4 scrollbar'>
+      <div
+        className={`col-span-3 overflow-auto p-4 scrollbar border-x-2 border-gray-400 bg-gray-900${
+          panelOpen ? '' : 'hidden'
+        } ${panelOpen ? 'absolute top-13 h-full right-0' : 'hidden'} ${
+          window.innerWidth > 622 ? 'absolute top-13 h-full right-0' : ''
+        }`}
+      >
         {/* botones */}
-        <div className='flex flex-wrap items-center justify-center gap-4 mt-2 mb-7'>
+        <div className='flex flex-wrap items-center justify-center gap-4 mt-10 mb-7'>
           <CreateGroupButton />
           <ButtonDropDown userContacts={allUsers} />
         </div>
@@ -60,7 +90,6 @@ const ProfilePage = () => {
           <h1 className='text-xs font-bold text-white'>Chats Privados</h1>
           <TiMessages className='text-white' />
         </div>
-
         <hr className='h-[1px] my-2 bg-gray-400 border-0' />
         {privateGroups?.length === 0 && (
           <div className='flex items-center justify-center text-xs font-bold'>
@@ -68,19 +97,16 @@ const ProfilePage = () => {
           </div>
         )}
         {privateGroups?.map((group) => (
-          <PrivateChatCard
-            key={group.id}
-            group={group}
-          />
+          <PrivateChatCard key={group.id} group={group} />
         ))}
         <hr className='h-[1px] my-2 bg-gray-400 border-0 mt-4' />
+
 
         {/* Paneles */}
         <div className='text-md flex items-center justify-around gap-3'>
           <h1 className='text-xs font-bold text-white'>Paneles de Grupo</h1>
           <LuLayoutPanelLeft className='text-white' />
         </div>
-
         <hr className='h-[1px] my-2 bg-gray-400 border-0' />
         {groups?.length === 0 && (
           <div className='flex items-center justify-center text-xs font-bold'>
@@ -88,18 +114,8 @@ const ProfilePage = () => {
           </div>
         )}
         {groups?.map((group) => (
-          <GroupChatCard
-            key={group.id}
-            group={group}
-          />
+          <GroupChatCard key={group.id} group={group} />
         ))}
-      </div>
-
-      {/* CHAT GRANDE */}
-      <div
-        className='col-span-9 border-x-2 border-gray-700 flex flex-col 
-      items-center justify-start mb-1 overflow-aut'>
-        <ChatPage />
       </div>
     </div>
   );
