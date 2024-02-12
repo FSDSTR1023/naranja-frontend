5dd38fd6f8fd41f05421334671cba29b61c3e9bc
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-02-12 21:33:16 +0100

    profile Page

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -59,11 +59,12 @@ const ProfilePage = () => {
       {/* Button to open/close the panel */}
       <button
         onClick={togglePanel}
-        className={`panel-toggle-button absolute mt-[0px] right-0 px-3 py-4 z-10 text-xs rounded-lg rounded-r-none ${
+        className={clsx(
+          `panel-toggle-button absolute mt-[0px] right-0 px-3 py-4 z-10 text-xs rounded-lg rounded-r-none`,
           panelOpen
             ? 'text-white'
             : 'bg-orange-500 text-white border-2 border-gray-200'
-        }`}>
+        )}>
         {panelOpen ? 'X' : 'Open'}
       </button>
 
