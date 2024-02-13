06054a5e614521aded3a32ffe1a86c481f7ab1f0
Kayra Czerniewicz T <31418061+Kayracz@users.noreply.github.com>
2024-02-13 10:49:19 -0400

    Responsiveness.
    correccion de la homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,8 +13,8 @@ const HomePage = () => {
         </h1>
         <h2 className='text-xl text-gray-900 max-w-6xl mx-auto'>
           Con TaskTalk puedes tener un grupo con cada equipo con el que trabajas, en él podrás <span> </span>
-          <span className="text-orange-400">chatear</span>, hacer <span className="font-bold text-orange-400"> videollamadas </span><br /> y tener un 
-          <span className="text-orange-400"> dashboard de tareas </span>
+          <span className="text-orange-400 font-bold">chatear</span>, hacer <span className="font-bold text-orange-400"> videollamadas </span><br /> y tener un 
+          <span className="text-orange-400 font-bold"> dashboard de tareas </span>
           con el que poder visualizar el progreso en tu proyecto. Comunícate de forma efectiva y utiliza el dashboard de tareas para sacar el máximo partido a tu equipo.
         </h2>
         {isAuthenticated ? <button onClick={() => { navigate('/profile-page') }} className='bg-orange-600 mt-8 text-white font-bold py-2 px-4 rounded-md hover:bg-orange-800'>Ve a tu perfil</button> : <button onClick={() => { navigate('/register-page') }} className='bg-orange-600 mt-8 text-white font-bold py-2 px-4 rounded-md hover:bg-orange-800'>Regístrate aquí</button>}
