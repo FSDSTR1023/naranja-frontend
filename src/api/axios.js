1fc9a595877bcd57b3a4a639b6d6dcfb6bc6e882
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-02-14 22:58:05 +0100

    quito puerto y cambio nombre variable de entorno

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_PAGE_URL || 'http://localhost:4000',
-  port: import.meta.env.VITE_PORT || 4000,
+  baseURL: import.meta.env.VITE_BASE_URL || 'http://localhost:4000',
+
   withCredentials: true,
 });
 
