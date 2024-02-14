5eed7f427fb95b6cde670045c649e16d88614322
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-02-14 22:52:33 +0100

    puerto en el ENV

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const instance = axios.create({
   baseURL: import.meta.env.VITE_PAGE_URL || 'http://localhost:4000',
+  port: import.meta.env.VITE_PORT || 4000,
   withCredentials: true,
 });
 
