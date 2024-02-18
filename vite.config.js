b07c7852b2d9c69ab73b00fc51eda02002148071
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-02-18 19:31:20 +0100

    cambio vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import process from 'process';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   define: {
-    'process.env': {},
+    'process.env': {
+      VITE_PUBLIC_LK_SERVER_URL: JSON.stringify(
+        process.env.VITE_PUBLIC_LK_SERVER_URL ||
+          'wss://team-chat-app-7keim926.livekit.cloud'
+      ),
+    },
   },
   test: {
     environment: 'happy-dom',
