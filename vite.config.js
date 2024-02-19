6341d21d0afb29d0598b539c9ab6b0df2fd93ac3
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-02-19 20:54:39 +0100

    cambios en el vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,15 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import process from 'process';
 
 // https://vitejs.dev/config/
-export default ({ mode }) =>
-  defineConfig({
-    plugins: [react()],
-    define: {
-      'process.env': {
-        ...loadEnv(mode, process.cwd()),
-        NODE_ENV: process.env.NODE_ENV,
-        LIVEKIT_API_KEY: process.env.LIVEKIT_API_KEY,
-        LIVEKIT_API_SECRET: process.env.LIVEKIT_API_SECRET,
-        PUBLIC_LIVEKIT_URL: process.env.PUBLIC_LIVEKIT_URL,
-      },
-    },
+export default defineConfig({
+  plugins: [react()],
+  define: {
+    'process.env': {},
+    'process.env.NODE_ENV': '"production"',
+  },
 
-    test: {
-      environment: 'happy-dom',
-    },
-  });
+  test: {
+    environment: 'happy-dom',
+  },
+});
