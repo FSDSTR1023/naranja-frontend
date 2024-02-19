623691b2607fc8ba7e3ece038d5bdfd3e1ba0e9e
22guerrabruno <139650855+22guerrabruno@users.noreply.github.com>
2024-02-19 18:50:57 +0100

    cambios en el vite.config

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,10 @@ export default defineConfig({
   define: {
     'process.env': {
       ...loadEnv('', process.cwd()),
-      return: 'process.env',
+      NODE_ENV: process.env.NODE_ENV,
+      LIVEKIT_API_KEY: process.env.LIVEKIT_API_KEY,
+      LIVEKIT_API_SECRET: process.env.LIVEKIT_API_SECRET,
+      PUBLIC_LIVEKIT_URL: process.env.PUBLIC_LIVEKIT_URL,
     },
   },
 
